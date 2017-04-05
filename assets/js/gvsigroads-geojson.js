dc90f331181200ff141fc3fb0ecadf93b3edca14
Pilar Argudo <pargudo@disid.com>
2017-04-05 18:46:23 +0200

    Added case of study map.

---
+++ assets/js/gvsigroads-geojson.js
@@ -0,0 +1,50 @@
+var RoadsCase = {
+"type": "FeatureCollection",
+"features": [
+    {
+      "geometry": {
+          "type": "Point",
+          "coordinates": [
+              -0.3743923,
+              39.4743929
+          ]
+      },
+      "type": "Feature",
+      "properties": {
+          "popupContent": "Valencia"
+      },
+      "id": 1
+    },
+    {
+      "geometry": {
+          "type": "Point",
+          "coordinates": [
+              -3.8319705,
+              37.780157
+          ]
+      },
+      "type": "Feature",
+      "properties": {
+          "popupContent": "Jaén"
+      },
+      "id": 2
+    },
+    {
+      "geometry": {
+          "type": "Point",
+          "coordinates": [
+              -5.6124967,
+              42.6036003
+          ]
+      },
+      "type": "Feature",
+      "properties": {
+          "popupContent": "León"
+      },
+      "id": 3
+    },
+
+  ]
+};
+
+
